c2d892efd5bf82a048eca72404f612aa9b979125
mfcRogue <roguegfh@gmail.com>
2021-02-02 17:18:07 +0000

    flexbox update

--- resources/views/reminder/dashboard.blade.php
+++ resources/views/reminder/dashboard.blade.php
@@ -5,27 +5,43 @@
         </h2>
     </x-slot>
 <div class="flex items-stretch flex-wrap">
-    <div class=" rounded overflow-hidden shadow-lg my-2 bg-gray-50 m-2">
-    <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">Service Reminders Due</div>
+    <div class="flex-grow rounded overflow-hidden shadow-lg my-2 bg-gray-50 m-2">
+    <div class="px-6 py-4  text-center">
+        <div class="font-bold text-xl mb-2"><livewire:vehicles.count.service-reminders /></div>
         <p class="text-grey-darker text-base ">
-         <livewire:vehicles.count.service-reminders /> Reminders due.
+         Due
         </p>
     </div>
     </div>
-    <div class="rounded overflow-hidden shadow-lg my-2 bg-gray-50 m-2">
-    <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">Service Reminders Ovdeue</div>
-        <p class="text-grey-darker text-base ">
-        X Reminders due in [Month]
+    <div class="flex-grow rounded overflow-hidden shadow-lg my-2 bg-gray-50 m-2">
+    <div class= "px-6 py-4  text-center">
+        <div class="font-bold text-xl mb-2"> x </div>
+        <p class="text-grey-darker text-base text-center">
+        Overdue
         </p>
     </div>
     </div>
-    <div class="rounded overflow-hidden shadow-lg my-2 bg-gray-50 m-2">
-    <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">Service Reminders To be Sent by Text</div>
-        <p class="text-grey-darker text-base ">
-            X Reminders to be sent in in [Month]
+    <div class="flex-grow rounded overflow-hidden shadow-lg my-2 bg-gray-50 m-2">
+    <div class="px-6 py-4  text-center">
+        <div class="font-bold text-xl mb-2"> x </div>
+        <p class="text-grey-darker text-base text-center">
+          Send by Text
+        </p>
+    </div>
+    </div>
+    <div class="flex-grow rounded overflow-hidden shadow-lg my-2 bg-gray-50 m-2">
+    <div class="flex-grow px-6 py-4  text-center">
+        <div class="font-bold text-xl mb-2"> x </div>
+        <p class="text-grey-darker text-base text-center">
+           Send by Email
+        </p>
+    </div>
+    </div>
+    <div class="flex-grow rounded overflow-hidden shadow-lg my-2 bg-gray-50 m-2">
+    <div class=" px-6 py-4  text-center">
+        <div class="font-bold text-xl mb-2"> x </div>
+        <p class="text-grey-darker text-base text-center">
+           Send by Post
         </p>
     </div>
     </div>
