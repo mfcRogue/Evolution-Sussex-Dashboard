8c6babfddd16f8e8ab43aa914958b553e305e7e6
mfcRogue <roguegfh@gmail.com>
2021-02-19 08:46:45 +0000

    update

--- app/Http/Livewire/Vehicles/Count/CombinedRemindersSendByEmail.php
+++ app/Http/Livewire/Vehicles/Count/CombinedRemindersSendByEmail.php
@@ -3,7 +3,7 @@
 namespace App\Http\Livewire\Vehicles\Count;
 
 use Livewire\Component;
-//use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Builder;
 use App\Models\Vehicle;
 use App\Models\Customer;
@@ -24,24 +24,17 @@ class CombinedRemindersSendByEmail extends Component
             $year = date('Y', strtotime(now()));
         }
 
+        $combined_email_send = 0;
+
         $vehicle = Vehicle::has('Customer')
+        ->select('ServDueDate', 'MOTDueDate', 'CustomerReference')
         ->whereBetween('ServDueDate', [$year.'-'.$month.'-01', $year.'-'.$month.'-31'])
         ->whereBetween('MOTDueDate', [$year.'-'.$month.'-01', $year.'-'.$month.'-31'])
         ->where('CustomerReference', '<>', 'INTERNAL')
-        //->get();
+        ->get();
         
-        $combined_email_send = 0;
-        foreach($vehicle as $veh_data)
-        {
-            
-            $cust_email = $veh_data->customer['Email'];
-            $cust_email2 = $veh_data->customer['Email2'];
-            if($cust_email <> '' or $cust_email2 <> '')
-            {
-                $combined_email_send++;
-            }
-       
-        }
+        //
+
         return view('livewire.vehicles.count.combined-reminders-send-by-email', ['combined_email_send'=>$combined_email_send]);
     }
 }
