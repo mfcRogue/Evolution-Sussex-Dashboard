c3454508885d16b7c52736b25e2f97c9622b8bf0
mfcRogue <roguegfh@gmail.com>
2021-02-24 16:32:09 +0000

    complete list due

--- app/Http/Controllers/ReminderController.php
+++ app/Http/Controllers/ReminderController.php
@@ -309,24 +309,33 @@ class ReminderController extends Controller
      */
     public function list_due($month)
     {
-        if($month == 12)
-        {
-            $year = date('Y', strtotime(now()->addYear()));    
-        }
-        else
-        {
-            $year = date('Y', strtotime(now()));
-        }
+        //$year = date('Y', strtotime(now()));
+        $year = 2020;
         
         $combined_data_due = DB::table('vehicles')
-        ->select('ServDueDate', 'MOTDueDate', 'customers.Email', 'customers.Email2', 'customers.Str1', 'customers.Name', 'customers.Title')
+        ->select('RegNo','ServDueDate', 'MOTDueDate', 'customers.Email', 'customers.Email2', 'customers.Str1', 'customers.Name', 'customers.Title')
         ->whereBetween('ServDueDate', [$year.'-'.$month.'-01', $year.'-'.$month.'-31'])
         ->whereBetween('MOTDueDate', [$year.'-'.$month.'-01', $year.'-'.$month.'-31'])
         ->where('CustomerReference', '<>', 'INTERNAL')
         ->join('customers', 'CustomerReference', '=', 'Reference')
         ->get();
+        $service_data_due = DB::table('vehicles')
+        ->select('RegNo', 'ServDueDate', 'MOTDueDate', 'customers.Email', 'customers.Email2', 'customers.Str1', 'customers.Name', 'customers.Title')
+        ->whereBetween('ServDueDate', [$year.'-'.$month.'-01', $year.'-'.$month.'-31'])
+        ->whereNotBetween('MOTDueDate', [$year.'-'.$month.'-01', $year.'-'.$month.'-31'])
+        ->where('CustomerReference', '<>', 'INTERNAL')
+        ->join('customers', 'CustomerReference', '=', 'Reference')
+        ->get();
+        $mot_data_due = DB::table('vehicles')
+        ->select('RegNo', 'ServDueDate', 'MOTDueDate', 'customers.Email', 'customers.Email2', 'customers.Str1', 'customers.Name', 'customers.Title')
+        ->whereNotBetween('ServDueDate', [$year.'-'.$month.'-01', $year.'-'.$month.'-31'])
+        ->whereBetween('MOTDueDate', [$year.'-'.$month.'-01', $year.'-'.$month.'-31'])
+        ->where('CustomerReference', '<>', 'INTERNAL')
+        ->join('customers', 'CustomerReference', '=', 'Reference')
+        ->get();
 
-        return view('reminder.list_due', ['month'=>$month, 'combined_data_due'=>$combined_data_due]);
+
+        return view('reminder.list_due', ['month'=>$month, 'combined_data_due'=>$combined_data_due, 'service_data_due'=>$service_data_due, 'mot_data_due'=>$mot_data_due]);
     }
 
 
--- resources/views/reminder/list_due.blade.php
+++ resources/views/reminder/list_due.blade.php
@@ -16,7 +16,15 @@
             </x-nav-link>
         </div>
     </x-slot>
-
+<div class="flex m-1  text-center  flex-wrap content-evenly">
+    <div class="flex-auto bg-gray-50 p-6 shadow-md rounded">
+        @for ($i = 1; $i < 13; $i++)
+        <x-nav-link :href="route('reminder.list.due', ['month'=>$i])">
+            {{ date('F', strtotime('2020-'. $i)) }}
+        </x-nav-link>
+        @endfor
+    </div>
+</div>
 <div class="flex m-1  text-center  flex-wrap content-evenly">
 <div class="flex-auto bg-gray-50 p-6 shadow-md rounded">
     <strong><div>Combined Service and MOT Reminders</div></strong>
@@ -27,39 +35,39 @@
 <table class="table-auto min-w-full divide-y divide-gray-200">
   <thead>
     <tr>
-      <th class="w-1/5">Name</th>
-      <th class="w-1/5">Service Reminder Date</th>
-      <th class="w-1/5">MOT Reminder Date</th>
-      <th class="w-1/5">To be Sent by</th>
-      <th class="w-1/5">Actions</th>
+      <th class="w-1/6">Name</th>
+      <th class="w-1/6">Registration Number</th>
+      <th class="w-1/6">Service Reminder Date</th>
+      <th class="w-1/6">MOT Reminder Date</th>
+      <th class="w-1/6">To be Sent by</th>
+      <th class="w-1/6">Actions</th>
     </tr>
   </thead>
   <tbody>
-    
+
   @foreach ($combined_data_due as $combined_records)
-  <tr>
-    <td>{{$combined_records->Title}} {{$combined_records->Name}}</td>
-    @php
-    $service_date = date('d-m-Y', strtotime($combined_records->ServDueDate));
-    $mot_date = date('d-m-Y', strtotime($combined_records->MOTDueDate));
-    @endphp
-    <td>{{$service_date}}</td>
-    <td>{{$mot_date}}</td>
+ @if ($loop->even)
+  <tr class="bg-gray-200">   
+  @else
+  <tr>   
+ @endif
+    <td class="text-left">{{$combined_records->Title}} {{$combined_records->Name}}</td>
+      <td>{{$combined_records->RegNo}}</td>
+
+    <td>{{date('d-m-Y', strtotime($combined_records->ServDueDate))}}</td>
+    <td>{{date('d-m-Y', strtotime($combined_records->MOTDueDate))}}</td>
     
     @if ($combined_records->Email <> '' or $combined_records->Email2 <> '')
-    <td><i class="fas fa-at"></i></td>    
+        <td><i class="fas fa-at"></i></td>    
     @elseif ($combined_records->Str1 <> '')
-    <td><i class="fas fa-sms"></i></td>
+        <td><i class="fas fa-sms"></i></td>
     @else
-     <td><i class="fas fa-mail-bulk"></i></td>
+        <td><i class="fas fa-mail-bulk"></i></td>
     @endif
-    <td>Edit | Delete | Valid?</td>
-     </tr>
+          <td>Edit | Delete | Valid?</td>
+    </tr>
   @endforeach
- 
- 
-
-    
+   
   </tbody>
 </table>
 </div>
@@ -71,8 +79,89 @@
 </div>
 <div class="flex m-1  text-center  flex-wrap content-evenly">
 <div class="flex-auto bg-gray-50 p-6 shadow-md rounded">
+<table class="table-auto min-w-full divide-y divide-gray-200">
+  <thead>
+    <tr>
+      <th class="w-1/6">Name</th>
+      <th class="w-1/6">Registration Number</th>
+      <th class="w-1/6">Service Reminder Date</th>
+      <th class="w-1/6">MOT Reminder Date</th>
+      <th class="w-1/6">To be Sent by</th>
+      <th class="w-1/6">Actions</th>
+    </tr>
+  </thead>
+  <tbody>
+
+  @foreach ($service_data_due as $service_records)
+ @if ($loop->even)
+  <tr class="bg-gray-200">   
+  @else
+  <tr>   
+ @endif
+    <td class="text-left">{{$service_records->Title}} {{$service_records->Name}}</td>
+    <td>{{$service_records->RegNo}}</td>
+
+    <td>{{date('d-m-Y', strtotime($service_records->ServDueDate))}}</td>
+    <td>{{date('d-m-Y', strtotime($service_records->MOTDueDate))}}</td>
+    
+    @if ($service_records->Email <> '' or $service_records->Email2 <> '')
+        <td><i class="fas fa-at"></i></td>    
+    @elseif ($service_records->Str1 <> '')
+        <td><i class="fas fa-sms"></i></td>
+    @else
+        <td><i class="fas fa-mail-bulk"></i></td>
+    @endif
+          <td>Edit | Delete | Valid?</td>
+    </tr>
+  @endforeach
+     </tbody>
+</table>
+</div>
+</div>
+<div class="flex m-1  text-center  flex-wrap content-evenly">
+<div class="flex-auto bg-gray-50 p-6 shadow-md rounded">
     <strong><div>MOT Reminders</div></strong>
 </div>
 </div>
+<div class="flex m-1  text-center  flex-wrap content-evenly">
+<div class="flex-auto bg-gray-50 p-6 shadow-md rounded">
+<table class="table-auto min-w-full divide-y divide-gray-200">
+  <thead>
+    <tr>
+      <th class="w-1/6">Name</th>
+      <th class="w-1/6">Registration Number</th>
+      <th class="w-1/6">Service Reminder Date</th>
+      <th class="w-1/6">MOT Reminder Date</th>
+      <th class="w-1/6">To be Sent by</th>
+      <th class="w-1/6">Actions</th>
+    </tr>
+  </thead>
+  <tbody>
 
+  @foreach ($mot_data_due as $mot_records)
+ @if ($loop->even)
+  <tr class="bg-gray-200">   
+  @else
+  <tr>   
+ @endif
+    <td class="text-left">{{$mot_records->Title}} {{$mot_records->Name}}</td>
+    <td>{{$mot_records->RegNo}}</td>
+
+    <td>{{date('d-m-Y', strtotime($mot_records->ServDueDate))}}</td>
+    <td>{{date('d-m-Y', strtotime($mot_records->MOTDueDate))}}</td>
+    
+    @if ($mot_records->Email <> '' or $mot_records->Email2 <> '')
+        <td><i class="fas fa-at"></i></td>    
+    @elseif ($mot_records->Str1 <> '')
+        <td><i class="fas fa-sms"></i></td>
+    @else
+        <td><i class="fas fa-mail-bulk"></i></td>
+    @endif
+          <td>Edit | Delete | Valid?</td>
+    </tr>
+  @endforeach
+     </tbody>
+</table>
+</div>
+</div>
 </x-app-layout>
