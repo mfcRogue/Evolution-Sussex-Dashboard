b45012c67bbecee2072d6aa789fc8e688c4f5448
mfcRogue <roguegfh@gmail.com>
2021-03-19 17:29:51 +0000

    update

--- app/Http/Controllers/ReminderSendController.php
+++ app/Http/Controllers/ReminderSendController.php
@@ -24,6 +24,11 @@ class ReminderSendController extends Controller
     {
         $year = date('Y', strtotime(now()));
 
+        //*******************
+        // 
+        // Combined service reminders
+        // 
+        //*******************
 
         $combined_data_due = DB::table('vehicles')
         ->join('customers', 'CustomerReference', '=', 'Reference')
@@ -50,11 +55,10 @@ class ReminderSendController extends Controller
             ->where('customers.Email2', '<>', '');
         })
         ->get();
-       //dump($combined_data_due);
 
-            foreach($combined_data_due as $mail_data)
-            {
-                $data = [
+        foreach($combined_data_due as $mail_data)
+        {
+            $data = [
                 'RegNo' => $mail_data->RegNo,
                 'Make' => $mail_data->Make,
                 'Model' => $mail_data->Model,
@@ -65,42 +69,47 @@ class ReminderSendController extends Controller
                 'CustTitle' => $mail_data->Title,
                 'Email1'=>$mail_data->Email,
                 'Email2'=>$mail_data->Email2
-                ];
-                //if customer has both emails set Email to "To" and Email2 as CC
-                if (!empty($mail_data->Email) and !empty($mail_data->Email2)) {
-                    Mail::to($mail_data->Email)
-                    ->cc($mail_data->Email2)
-                    ->bcc("service@evosussex.co.uk")
-                    ->send(new CombinedReminder($data));
+            ];
+            //if customer has both emails set Email to "To" and Email2 as CC
+            if (!empty($mail_data->Email) and !empty($mail_data->Email2)) {
+                Mail::to($mail_data->Email)
+                ->cc($mail_data->Email2)
+                ->bcc("service@evosussex.co.uk")
+                ->send(new CombinedReminder($data));
 
-                   
-                }
-                //else if Email 2 is only set set Email2
-                elseif(empty($mail_data->Email) and !empty($mail_data->Email2))
-                {
-                    Mail::to($mail_data->Email2)
-                    ->bcc("service@evosussex.co.uk")
-                    ->send(new CombinedReminder($data));
-
-                }
-                elseif(!empty($mail_data->Email) and empty($mail_data->Email2))
-                {
-                    Mail::to($mail_data->Email)
-                    ->bcc("service@evosussex.co.uk")
-                    ->send(new CombinedReminder($data));
-                }
-                //use return render to view email and testing
-                //return (new CombinedReminder($data))->render();
                 
-                //use to send mail via .env settings
-
-                unset($data);
-                unset($mail_data);
-  
             }
+            //else if Email 2 is only set set Email2
+            elseif(empty($mail_data->Email) and !empty($mail_data->Email2))
+            {
+                Mail::to($mail_data->Email2)
+                ->bcc("service@evosussex.co.uk")
+                ->send(new CombinedReminder($data));
+
+            }
+            elseif(!empty($mail_data->Email) and empty($mail_data->Email2))
+            {
+                Mail::to($mail_data->Email)
+                ->bcc("service@evosussex.co.uk")
+                ->send(new CombinedReminder($data));
+            }
+            //use return render to view email and testing
+            //return (new CombinedReminder($data))->render();
+            
+            //use to send mail via .env settings
+
+            unset($data);
+            unset($mail_data);
+
+        }
            
 
-            
+        //*******************
+        // 
+        // Service only service reminders
+        // 
+        //*******************
+
         $service_data_due = DB::table('vehicles')
         ->join('customers', 'CustomerReference', '=', 'Reference')
         ->select('RegNo','Make','Model','ServDueDate', 'MOTDueDate', 'customers.Email', 'customers.Email2', 'customers.Str1', 'customers.Name', 'customers.Title', 'customers.Forename')
@@ -128,8 +137,8 @@ class ReminderSendController extends Controller
         ->get();
 
         foreach($service_data_due as $mail_data)
-            {
-                $data = [
+        {
+            $data = [
                 'RegNo' => $mail_data->RegNo,
                 'Make' => $mail_data->Make,
                 'Model' => $mail_data->Model,
@@ -140,92 +149,117 @@ class ReminderSendController extends Controller
                 'CustTitle' => $mail_data->Title,
                 'Email1'=>$mail_data->Email,
                 'Email2'=>$mail_data->Email2
-                ];
-                //if customer has both emails set Email to "To" and Email2 as CC
-                if (!empty($mail_data->Email) and !empty($mail_data->Email2)) {
+            ];
+            //if customer has both emails set Email to "To" and Email2 as CC
+            if (!empty($mail_data->Email) and !empty($mail_data->Email2)) 
+            {
 
+                Mail::to($mail_data->Email)
+                ->cc($mail_data->Email2)
+                ->bcc("service@evosussex.co.uk")
+                ->send(new ServiceReminder($data));     
 
-                }
-                //else if Email 2 is only set set Email2
-                elseif(empty($mail_data->Email) and !empty($mail_data->Email2))
-                {
-
-                }
-                else{
-
-                }
-                //use return render to view email and testing
-                //return (new ServiceReminder($data))->render();
-                
-                //use to send mail via .env settings
-  
             }
-            /*Mail::to("roguegfh@gmail.com")
-            ->bcc("service@evosussex.co.uk")
-            ->send(new CombinedReminder($data));*/
+            //else if Email 2 is only set set Email2
+            elseif(empty($mail_data->Email) and !empty($mail_data->Email2))
+            {
 
-            $mot_data_due = DB::table('vehicles')
-            ->join('customers', 'CustomerReference', '=', 'Reference')
-            ->select('RegNo','Make','Model','ServDueDate', 'MOTDueDate', 'customers.Email', 'customers.Email2', 'customers.Str1', 'customers.Name', 'customers.Title', 'customers.Forename')
+                Mail::to($mail_data->Email2)
+                ->bcc("service@evosussex.co.uk")
+                ->send(new ServiceReminder($data));
+
+            }
+            elseif(!empty($mail_data->Email) and empty($mail_data->Email2))
+            {
+
+                Mail::to($mail_data->Email)
+                ->bcc("service@evosussex.co.uk")
+                ->send(new ServiceReminder($data));
+
+            }
+            //use return render to view email and testing
+            //return (new ServiceReminder($data))->render();
+            
+        }
+
+        //*******************
+        // 
+        // MOT only service reminders
+        // 
+        //*******************
+
+        $mot_data_due = DB::table('vehicles')
+        ->join('customers', 'CustomerReference', '=', 'Reference')
+        ->select('RegNo','Make','Model','ServDueDate', 'MOTDueDate', 'customers.Email', 'customers.Email2', 'customers.Str1', 'customers.Name', 'customers.Title', 'customers.Forename')
+        ->whereNotBetween('ServDueDate', [$year.'-'.$month.'-01', $year.'-'.$month.'-31'])
+        ->whereBetween('MOTDueDate', [$year.'-'.$month.'-01', $year.'-'.$month.'-31'])
+        ->where('CustomerReference', '<>', 'INTERNAL')
+        ->where('customers.Email', '<>', '')
+        ->where('customers.Email2', '<>', '')
+        ->orWhere(function($query) use($month, $year) {
+            $query
             ->whereNotBetween('ServDueDate', [$year.'-'.$month.'-01', $year.'-'.$month.'-31'])
             ->whereBetween('MOTDueDate', [$year.'-'.$month.'-01', $year.'-'.$month.'-31'])
             ->where('CustomerReference', '<>', 'INTERNAL')
             ->where('customers.Email', '<>', '')
-            ->where('customers.Email2', '<>', '')
-            ->orWhere(function($query) use($month, $year) {
-                $query
-                ->whereNotBetween('ServDueDate', [$year.'-'.$month.'-01', $year.'-'.$month.'-31'])
-                ->whereBetween('MOTDueDate', [$year.'-'.$month.'-01', $year.'-'.$month.'-31'])
-                ->where('CustomerReference', '<>', 'INTERNAL')
-                ->where('customers.Email', '<>', '')
-                ->where('customers.Email2', '=', '');
-            })
-            ->orWhere(function($query) use($month, $year) {
-                $query
-                ->whereNotBetween('ServDueDate', [$year.'-'.$month.'-01', $year.'-'.$month.'-31'])
-                ->whereBetween('MOTDueDate', [$year.'-'.$month.'-01', $year.'-'.$month.'-31'])
-                ->where('CustomerReference', '<>', 'INTERNAL')
-                ->where('customers.Email', '=', '')
-                ->where('customers.Email2', '<>', '');
-            })
-            ->get();
-    
-            foreach($mot_data_due as $mail_data)
-                {
-                    $data = [
-                    'RegNo' => $mail_data->RegNo,
-                    'Make' => $mail_data->Make,
-                    'Model' => $mail_data->Model,
-                    'ServDueDate' => date('d/m/Y', strtotime($mail_data->ServDueDate)),
-                    'MOTDueDate' => date('d/m/Y', strtotime($mail_data->MOTDueDate)),
-                    'CustName' => $mail_data->Name,
-                    'CustForename' => $mail_data->Forename,
-                    'CustTitle' => $mail_data->Title,
-                    'Email1'=>$mail_data->Email,
-                    'Email2'=>$mail_data->Email2
-                    ];
-                    //if customer has both emails set Email to "To" and Email2 as CC
-                    if (!empty($mail_data->Email) and !empty($mail_data->Email2)) {
-    
-    
-                    }
-                    //else if Email 2 is only set set Email2
-                    elseif(empty($mail_data->Email) and !empty($mail_data->Email2))
-                    {
-    
-                    }
-                    else{
-    
-                    }
-                    //use return render to view email and testing
-                    //return (new ServiceReminder($data))->render();
-                    
-                    //use to send mail via .env settings
-      
-                }
-                /*Mail::to("roguegfh@gmail.com")
+            ->where('customers.Email2', '=', '');
+        })
+        ->orWhere(function($query) use($month, $year) {
+            $query
+            ->whereNotBetween('ServDueDate', [$year.'-'.$month.'-01', $year.'-'.$month.'-31'])
+            ->whereBetween('MOTDueDate', [$year.'-'.$month.'-01', $year.'-'.$month.'-31'])
+            ->where('CustomerReference', '<>', 'INTERNAL')
+            ->where('customers.Email', '=', '')
+            ->where('customers.Email2', '<>', '');
+        })
+        ->get();
+
+        foreach($mot_data_due as $mail_data)
+        {
+            $data = [
+                'RegNo' => $mail_data->RegNo,
+                'Make' => $mail_data->Make,
+                'Model' => $mail_data->Model,
+                'ServDueDate' => date('d/m/Y', strtotime($mail_data->ServDueDate)),
+                'MOTDueDate' => date('d/m/Y', strtotime($mail_data->MOTDueDate)),
+                'CustName' => $mail_data->Name,
+                'CustForename' => $mail_data->Forename,
+                'CustTitle' => $mail_data->Title,
+                'Email1'=>$mail_data->Email,
+                'Email2'=>$mail_data->Email2
+            ];
+            //if customer has both emails set Email to "To" and Email2 as CC
+            if (!empty($mail_data->Email) and !empty($mail_data->Email2)) 
+            {
+
+                Mail::to($mail_data->Email)
+                ->cc($mail_data->Email2)
                 ->bcc("service@evosussex.co.uk")
-                ->send(new CombinedReminder($data));*/
+                ->send(new MOTReminder($data));
+
+            
+            }
+            //else if Email 2 is only set set Email2
+            elseif(empty($mail_data->Email) and !empty($mail_data->Email2))
+            {
+
+                Mail::to($mail_data->Email2)
+                ->bcc("service@evosussex.co.uk")
+                ->send(new MOTReminder($data));
+
+            }
+            elseif(!empty($mail_data->Email) and empty($mail_data->Email2))
+            {
+
+                Mail::to($mail_data->Email)
+                ->bcc("service@evosussex.co.uk")
+                ->send(new MOTReminder($data));
+                
+            }
+            //use return render to view email and testing
+            //return (new ServiceReminder($data))->render();
+
+        }
 
     }
 
--- resources/views/mail/reminder/mot.blade.php
+++ resources/views/mail/reminder/mot.blade.php
@@ -1,13 +1,14 @@
 <div><img src='https://evolutionsussex.co.uk/public/img/header.jpg' width='80%'/></div>
 
 <div class="font-sans">
+
 <p>Dear {{$data['CustTitle']}} {{$data['CustForename']}} {{$data['CustName']}}</p>
 <p>Your {{$data['Make']}} {{$data['Model']}} - {{$data['RegNo']}}</p>
 
-<p>Is due its service around the <strong>{{$data['ServDueDate']}}</strong></p> 
+<p>Is due its MOT before the <strong>{{$data['MOTDueDate']}}</strong></p> 
 
 
-<p>Should you wish us to undertake the service please do not hesitate to contact David the Service Advisor, and he will be more than happy to book your car in for you.</p>
+<p>Should you wish us to undertake the MOT please do not hesitate to contact David the Service Advisor, and he will be more than happy to book your car in for you.</p>
 
 <p>You can contact David on: <a href="tel:01273388804">01273 388804</a> by Email: <a href="mailto:service@evosussex.co.uk">service@evosussex.co.uk</a> or by text: <a href="sms:07507332161">07507 332161</a></p>
 
