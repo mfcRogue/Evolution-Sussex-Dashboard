b11fef5b511c3a0fcd1438df842c702d37e9ffc4
mfcRogue <roguegfh@gmail.com>
2021-03-29 10:07:35 +0100

    complete accounts

--- app/Http/Controllers/AccountsController.php
+++ app/Http/Controllers/AccountsController.php
@@ -9,6 +9,9 @@ use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Str;
 //use database
 use Illuminate\Support\Facades\DB;
+//use account CSV model
+use App\Models\AccountCSV;
+
 
 
 
@@ -37,25 +40,22 @@ class AccountsController extends Controller
 
         $fp =  fopen('../storage/app/'. $path, 'r');
        
-        dump($name, $path);
-
-        //$path = $request->file('file')->getRealPath();
-       // $data = array_map('str_getcsv', file($path));
-        //$csv_data = array_slice($data, 0, 1000);
+        $data = array_map('str_getcsv', file('../storage/app/'. $path));
+      
+        $csv_data = array_slice($data, 0, 1000);
   
         
-       // foreach($csv_data as $csv)
-        //{
-        //    $contains =  Str::contains($csv['1'], "Inverted");
+        foreach($data as $csv)
+        {
+            $contains =  Str::contains($csv['1'], "Inverted");
 
-        //    if($contains)
-       //     {
-       //         $csv['2'] = $csv['2'] * -1;
-       //         $csv['3'] = $csv['3'] * -1;
-               
-        //    }
+            if($contains)
+            {
+                $csv['2'] = $csv['2'] * -1;
+                $csv['3'] = $csv['3'] * -1;               
+            }
             
-            /*DB::table('accounts')->insert([
+            DB::table('accounts')->insert([
                 'col0' =>  $csv['0'],
                 'col1' =>  $csv['1'],
                 'col2' =>  $csv['2'],
@@ -63,15 +63,24 @@ class AccountsController extends Controller
                 'col4' =>  $csv['4'],
 
             ]);
-            */
+            
 
-        //}
+        }
+        $data = AccountCSV::get();
+
+        $csvExporter = new \Laracsv\Export();
+        $csvExporter->build($data, ['col0', 'col1', 'col2', 'col3', 'col4'], [
+            'header' => false,
+        ]);
+        $csvExporter->download($name);
+        DB::table('accounts')->truncate();
+
         fclose($fp);
         $delete = Storage::disk('local')->delete($path);
-        dump($delete);
+        //dump($delete);
 
    
-         //return redirect('account.proccess',['file'=>$path]);
+        return redirect()->back();
         
     }
 
