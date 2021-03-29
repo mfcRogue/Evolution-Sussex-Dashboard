6155922130ba6cb0029f8817b27cf5671b1522d3
mfcRogue <roguegfh@gmail.com>
2021-03-29 11:49:53 +0100

    initial route to dahsboard
    req login at all times.

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,8 @@ use App\Http\Controllers\AccountsController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
